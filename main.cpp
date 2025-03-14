501a11468ccd4cf5ce4a26cb311ca0e1a64ea10b
Dumitru Vlad-Mihai <vlad.liviu22@gmail.com>
2025-03-14 17:52:23 +0200

    Implement Display Functionality

--- main.cpp
+++ main.cpp
@@ -15,7 +15,11 @@ class CryptoCurrency {
         float get_amount() { return this->amount; }
         float get_unitPrice() { return this->unitPrice; }
 
-
+        void display() const{
+            std::cout << "Name: " << name << " ,Amount held: " 
+                      << amount << " ,Unit price: " << unitPrice << std::endl;
+        }
+        
 };
 
 class Wallet {
@@ -27,13 +31,70 @@ class Wallet {
         Wallet(float balance, int cryptoAssets, std::vector<CryptoCurrency> coins)
         : balance(balance), cryptoAssets(cryptoAssets), coins(coins) {}
 
-        
+        void displayWallet() {
+            std::cout << "\nAvailable Balance: " << balance << std::endl;
+            for(int i=0; i<cryptoAssets; i++){
+                coins[i].display();
+            }
+        }
+
 
 };
 
 int main() {
 
+    float temp_balance;
 
+    std::cout << "Enter the initial Balance: ";
+    std::cin >> temp_balance;
+    std::cin.ignore();
 
-    return 0;
+    int temp_cryptoAssets;
+    std::vector<CryptoCurrency> coins;
+
+    std::cout << "Types of Crypto Assets: ";
+    std::cin >> temp_cryptoAssets;
+    std::cin.ignore();
+
+    for(int i=0; i<temp_cryptoAssets; i++){
+        std::string temp_name;
+        float temp_amount;
+        float temp_unitPrice;
+
+        std::cout << "Asset's name: ";
+        std::getline(std::cin, temp_name);
+        std::cout << "Asset amount: ";
+        std::cin >> temp_amount;
+        std::cout << "Unit price: ";
+        std::cin >> temp_unitPrice;
+        std::cin.ignore();
+
+        coins.emplace_back(temp_name, temp_amount, temp_unitPrice);
+    }
+
+    Wallet wallet(temp_balance, temp_cryptoAssets, coins);
+
+    int option;
+    
+    do {
+        std::cout << "\nSelect an Option:\n";
+        std::cout << "1. Wallet info\n";
+        std::cout << "2. -\n";
+        std::cout << "3. -\n";
+        std::cout << "4. -\n";
+        std::cout << "0. Exit\n";
+        std::cout << "Option: ";
+        std::cin >> option;
+
+        switch(option){
+            case 1:
+                wallet.displayWallet();
+                break;
+
+            case 0:
+                std::cout << "Exiting...\n";
+                break;
+        }
+    } while (option != 0);
+
 }
